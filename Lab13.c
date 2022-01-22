652508dd3b7fc0b9931b97a4a83e08e971804121
EndsCreed <jonjondm2001@gmail.com>
2022-01-22 00:14:43 -0330

    Part 3 completed

--- Lab13.c
+++ Lab13.c
@@ -2,7 +2,18 @@
 // Created by Jonathan Mitchell on 1/22/22
 // 20160512
 
+#include <stdio.h>
+
 int main(int argc, char **argv) {
+    int i = 1;
+    int sum = 0;
+
+    while (i <= 15) {
+        sum += i;
+        i++;
+    }
+
+    printf("Sum of first 15 natural numbers: %i", sum);
 
     return 0;
 }
